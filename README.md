a907c50148d105eff0ff64a7fd3a6c304ec55df5
himag09 <imanol125@hotmail.com>
2025-04-22 20:46:07 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Ejercicio
+--------------
+3. Problemas prácticos: Conceptos básicos de Compose
+* Ejercicio 3-b Administrador de tareas
+--------------
+Capturas:
+--------------
---
+++ app/src/main/java/com/himag09/taskadministratorejercicio3b/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.himag09.taskadministratorejercicio3b
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.himag09.taskadministratorejercicio3b.ui.theme.TaskAdministratorEjercicio3bTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TaskAdministratorEjercicio3bTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    TaskCompleted(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskCompleted(modifier: Modifier = Modifier) {
+    val imageCompleted = painterResource(R.drawable.ic_task_completed)
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+    ) {
+        Image(painter = imageCompleted, contentDescription = null)
+        Text(
+            text = "All tasks completed",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, end = 8.dp )
+        )
+        Text(
+            text = "Nice work!",
+            fontSize = 16.sp,
+            modifier = Modifier
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TaskAdministratorEjercicio3bTheme {
+        TaskCompleted()
+    }
+}
