defa87ca36bc903ae4064edb9eb2b55d692e7fe1
himag09 <imanol125@hotmail.com>
2025-04-22 20:53:49 -0300

    changed text to resource

--- app/src/main/java/com/himag09/taskadministratorejercicio3b/MainActivity.kt
+++ app/src/main/java/com/himag09/taskadministratorejercicio3b/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,12 +52,12 @@ fun TaskCompleted(modifier: Modifier = Modifier) {
     ) {
         Image(painter = imageCompleted, contentDescription = null)
         Text(
-            text = "All tasks completed",
+            text = stringResource(R.string.string_all_task_completed),
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(top = 24.dp, end = 8.dp )
         )
         Text(
-            text = "Nice work!",
+            text = stringResource(R.string.string_nice_work),
             fontSize = 16.sp,
             modifier = Modifier
         )
